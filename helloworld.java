9c0b4fe7d4db989d3f35b3135cb3340ab6bf8273
Shrey Joshi <shreyjoshi29@gmail.com>
2023-11-14 16:33:09 -0500

    initial commit

---
+++ helloworld.java
@@ -0,0 +1,12 @@
+// HelloWorld.java
+// HelloWorld.java
+public class HelloWorld {
+    public String getGreeting() {
+        return "Hello, Jenkins!";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new HelloWorld().getGreeting());
+    }
+}
+
